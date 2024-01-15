366584681974138e88613a7a15b347a32a85241d
odinnlol <90418059+odinnlol@users.noreply.github.com>
2024-01-15 23:13:40 +0100

    cc

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,6 +1,16 @@
-#!/usr/bin/python3
-def element_at(my_list, index):
-    if index < 0 or index > len(my_list) - 1:
-        return 'None'
-    else:
-        return my_list[index]
+import re
+
+def extract_ips(filename):
+    # Regular expression pattern for matching IP addresses
+    ip_pattern = r'\b(?:\d{1,3}\.){3}\d{1,3}\b'
+    
+    with open(filename, 'r') as file:
+        for line in file:
+            # Find all IP addresses in the line
+            ips = re.findall(ip_pattern, line)
+            
+            for ip in ips:
+                print(ip)
+
+# Using the provided filename
+extract_ips('ips.txt')
